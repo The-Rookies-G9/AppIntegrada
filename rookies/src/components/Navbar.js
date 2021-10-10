4c4a8f8affda2bb2bdd2936c9eaf1e240a6170da
JESUS <ultrasnoop@yahoo.es>
2021-10-10 16:36:57 -0500

    prueba 1 menu navbar

--- rookies/src/components/Navbar.js
+++ rookies/src/components/Navbar.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+
 const Navbar = () => {
     return (
         <div>
+            <ul class="navbar">
+                <li>
+                    Logo   
+                  
+
+
+                </li>
+                <li>
+                    Admin            
+                </li>
+                <li>
+                    Productos            
+                </li>
+                <li>
+                    Roles            
+                </li>
+                <li>
+                    Ventas           
+                </li>
+                <li>
+                    Login            
+                </li>
+
+            </ul>
             Este es el navbar o header para las paginas de las secciones 
         </div>
     )
