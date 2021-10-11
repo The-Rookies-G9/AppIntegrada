74b3b521122a9ebf3c7c07d7c08bd2d756c3c643
JESUS <ultrasnoop@yahoo.es>
2021-10-11 00:56:42 -0500

    footer fix e index con carrusel no terminado aun

--- rookies/src/components/Navbar.js
+++ rookies/src/components/Navbar.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import Logo from './Logo.png';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDt1RxoybACqJNQXBafGK-zNnu9hR30Bj0",
+    authDomain: "oceanic-camera-328004.firebaseapp.com",
+    projectId: "oceanic-camera-328004",
+    storageBucket: "oceanic-camera-328004.appspot.com",
+    messagingSenderId: "663883928009",
+    appId: "1:663883928009:web:41ea9fb69ae13fec7f9818",
+    measurementId: "G-MP0KM0PW8L"
+  };
+
+
+
+
+
+
+
+
+
 
 
 const Navbar = () => {
     return (
         <div>
             <ul class="navbar">
-                <li className="text-uppercase font-weight-bold">
-                    <a href="#!">LOGO</a>
+                <li>
+                    <a class="navbar-brand" href="#">
+                        <img
+                            src= {Logo}
+                            height="70"
+                            alt=""
+
+                        />
+                    </a>
                 </li>
+
+                <hr/>
  
                 <li className="text-uppercase font-weight-bold ">
                 <a path ="/Admin/Admin">ADMIN</a>
@@ -25,11 +56,39 @@ const Navbar = () => {
                 <a path ="/Admin/Vtas">VENTAS</a>
                 </li>     
 
-                <li className="text-uppercase font-weight-bold">
-                <a href="#">LOGIN</a>
+                <hr/>
+
+                <li>
+                <script src="https://accounts.google.com/gsi/client" async defer></script>
+                <div class="g_id_signin"
+                    data-type="standard"
+                    data-shape="rectangular"
+                    data-theme="filled_blue"
+                    data-text="$ {button.text}"
+                    data-size="medium"
+                    data-locale="es-419"
+                    data-logo_alignment="left">
+                        
+                </div>
+                <div id="g_id_onload"
+                    data-client_id="663883928009-htpqqnqu74irl4334peh07b6lve7vj96.apps.googleusercontent.com"
+                    data-context="signin"
+                    data-ux_mode="popup"
+                    data-login_uri="https://localhost:3001"
+                    data-native_login_uri="https://localhost:3001">
+
+                </div>
+
+
+
+                <div class="g_id_signout"><h7>Cerrar Sesión</h7></div>
+      
+
+
+
                 </li>     
             </ul>
-            Este es el navbar o header para las paginas de las secciones 
+            
         </div>
     )
 }
--- rookies/src/pages/Indexpage.js
+++ rookies/src/pages/Indexpage.js
@@ -3,18 +3,48 @@ import '@fortawesome/fontawesome-free/css/all.min.css'; import
 'bootstrap-css-only/css/bootstrap.min.css'; import
 'mdbreact/dist/css/mdb.css';
 
+
+import {
+    MDBCarousel,
+    MDBCarouselInner,
+    MDBCarouselItem,
+    MDBCarouselElement,
+    MDBCarouselCaption,
+  } from 'mdb-react-ui-kit';
+  
+
+
+
 const Index = () => {
     return (
-        <div>
-        Página de contenido landing page, aqui va el codigo de la pagina
-        Página de contenido landing page, aqui va el codigo de la pagina
-        Página de contenido landing page, aqui va el codigo de la pagina
-        Página de contenido landing page, aqui va el codigo de la pagina
-        Página de contenido landing page, aqui va el codigo de la pagina
-        Página de contenido landing page, aqui va el codigo de la pagina
-        
-        </div>
-    )
-}
+        <MDBCarousel showIndicators showControls fade>
+        <MDBCarouselInner>
+          <MDBCarouselItem itemId={0}>
+            <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg' alt='...' />
+            <MDBCarouselCaption>
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+  
+          <MDBCarouselItem itemId={1}>
+            <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg' alt='...' />
+            <MDBCarouselCaption>
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+  
+          <MDBCarouselItem itemId={2}>
+            <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg' alt='...' />
+            <MDBCarouselCaption>
+              <h5>Third slide label</h5>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    );
+  }
 
 export default Index;
