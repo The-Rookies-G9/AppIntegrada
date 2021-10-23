c0b2f5ca37f99d599fdb4303aea6e28d5d1420d6
Maria <mafeescobar3110@gmail.com>
2021-10-23 18:31:12 -0500

    Productos y Roles

---
+++ rookies/src/components/Usuario/index.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from `react`;
+const Clientes = () =>{
+    const
+    return(
+        <table clasName="Lista de clientes">
+            <thead>
+                <tr>
+                    <th scope="col">Num</th>
+                    <th scope="col">Nombre producto</th>
+                    <th scope="col">Num</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Num</th>
+                    <td>Mark </td>
+                    <td>otto</td>
+                    <td>@gmail</td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark </td>
+                    <td>otto</td>
+                    <td>@gmail</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Mark </td>
+                    <td>otto</td>
+                    <td>@gmail</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Mark </td>
+                    <td>otto</td>
+                    <td>@gmail</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
